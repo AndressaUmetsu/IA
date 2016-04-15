d8377af6a31f596bd556f6a21ad364cd3759643a
AndressaUmetsu <andressa.umetsu@gmail.com>
2016-04-15 12:49:06 -0300

    Roboxinho gay da japa

---
+++ roboxinho/nome.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+#include <iostream>
+#include <cstdio>
+
+int main(int argc, char const *argv[])
+{
+    FILE *file = fopen("Robo_ambiente.txt", "rt");
+    int width = 42;
+    int heigth = 42;
+
+    int i;
+
+    for ( i = 0 ; i < 42 ; i++ ){
+    }
+
+    return 0;
+}
