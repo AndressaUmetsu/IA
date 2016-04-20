8d04260876e1c237056fba31c1d128abb33e210c
h3nnn4n <uber.renan@gmail.com>
2016-04-20 07:06:08 -0300

    Fixed zero cost

--- roboxinho/cu.cpp
+++ roboxinho/cu.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 #include <cstdio>
 #include <cstdlib>
-#include <algorithm> 
+#include <algorithm>
 #include <iostream>
 
 bool CompareCu ( _square a, _square b){
@@ -41,7 +41,7 @@ void Cu ( _square** map, _pos start, _pos stop ){
         	}	
 
             if ( lugar.y > 0 && !map[lugar.x][lugar.y-1].visited ){
-                map[lugar.x][lugar.y-1].cu = map[lugar.x][lugar.y].cu + map[lugar.x][lugar.y-1].type * (map[lugar.x][lugar.y-1].type > 1 ? map[lugar.x][lugar.y-1].type * 5 : 1);
+                map[lugar.x][lugar.y-1].cu = map[lugar.x][lugar.y].cu + (map[lugar.x][lugar.y-1].type + 1) * ((map[lugar.x][lugar.y-1].type + 1) > 1 ? (map[lugar.x][lugar.y-1].type + 1) * 5 : 1);
                 map[lugar.x][lugar.y-1].xx = lugar.x;
                 map[lugar.x][lugar.y-1].yy = lugar.y;
                 map[lugar.x][lugar.y-1].visited = true;
@@ -49,8 +49,8 @@ void Cu ( _square** map, _pos start, _pos stop ){
                 u.push_back( map[lugar.x][lugar.y-1] );
             }
 
-            if ( lugar.x > 0 && !map[lugar.x-1][lugar.y].visited){  
-                map[lugar.x-1][lugar.y].cu = map[lugar.x][lugar.y].cu + map[lugar.x-1][lugar.y].type * (map[lugar.x-1][lugar.y].type > 1 ? map[lugar.x-1][lugar.y].type * 5 : 1);
+            if ( lugar.x > 0 && !map[lugar.x-1][lugar.y].visited){
+                map[lugar.x-1][lugar.y].cu = map[lugar.x][lugar.y].cu + (map[lugar.x-1][lugar.y].type + 1) * ((map[lugar.x-1][lugar.y].type + 1) > 1 ? (map[lugar.x-1][lugar.y].type + 1) * 5 : 1);
                 map[lugar.x-1][lugar.y].xx = lugar.x;
                 map[lugar.x-1][lugar.y].yy = lugar.y;
                 map[lugar.x-1][lugar.y].visited = true;
@@ -59,15 +59,16 @@ void Cu ( _square** map, _pos start, _pos stop ){
             }
 
             if ( lugar.x < 41 && !map[lugar.x+1][lugar.y].visited){
-                map[lugar.x+1][lugar.y].cu = map[lugar.x][lugar.y].cu + map[lugar.x+1][lugar.y].type * (map[lugar.x+1][lugar.y].type > 1 ? map[lugar.x+1][lugar.y].type * 5 : 1);
+                map[lugar.x+1][lugar.y].cu = map[lugar.x][lugar.y].cu + (map[lugar.x+1][lugar.y].type + 1) * ((map[lugar.x+1][lugar.y].type + 1) > 1 ? (map[lugar.x+1][lugar.y].type + 1) * 5 : 1);
                 map[lugar.x+1][lugar.y].xx = lugar.x;
                 map[lugar.x+1][lugar.y].yy = lugar.y;
                 map[lugar.x+1][lugar.y].visited = true;
+
                 u.push_back( map[lugar.x+1][lugar.y] );
             }
 
             if ( lugar.y < 41 && !map[lugar.x][lugar.y+1].visited){
-                map[lugar.x][lugar.y+1].cu = map[lugar.x][lugar.y].cu + map[lugar.x][lugar.y+1].type * (map[lugar.x][lugar.y+1].type > 1 ? map[lugar.x][lugar.y+1].type * 5 : 1);
+                map[lugar.x][lugar.y+1].cu = map[lugar.x][lugar.y].cu + (map[lugar.x][lugar.y+1].type + 1) * ((map[lugar.x][lugar.y+1].type + 1) > 1 ? (map[lugar.x][lugar.y+1].type + 1) * 5 : 1);
                 map[lugar.x][lugar.y+1].xx = lugar.x;
                 map[lugar.x][lugar.y+1].yy = lugar.y;
                 map[lugar.x][lugar.y+1].visited = true;
@@ -81,11 +82,11 @@ void Cu ( _square** map, _pos start, _pos stop ){
         	break;
 
         std::sort (u.begin(), u.end(), CompareCu);
-        
+
         c = u;
         u.clear();
     }
-    
+
     printf("%d %d\n",lugar.x, lugar.y );
 
     int cost = 0;
@@ -94,7 +95,7 @@ void Cu ( _square** map, _pos start, _pos stop ){
         _square aux = lugar;
         printf("%d\n",map[aux.x][aux.y].cu );
         map[aux.x][aux.y].path = true;
-        cost += map[aux.x][aux.y].cu; 
+        cost += map[aux.x][aux.y].cu;
         lugar.x = map[aux.x][aux.y].xx;
         lugar.y = map[aux.x][aux.y].yy;
     }
