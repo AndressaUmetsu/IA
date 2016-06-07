cd9bf3c5047e22a1d748a320bee3426b13fc9069
h3nnn4n <uber.renan@gmail.com>
2016-06-07 07:56:27 -0300

    Fixed drawing factory and itens

--- robo_tunado/map.cpp
+++ robo_tunado/map.cpp
@@ -64,62 +64,64 @@ void printfImage(_square **map, _robo *robo, _fabrica* fabrica, int iter ){
                 fprintf(out, "\" r=\"%d\" stroke=\"black\" stroke-width=\"1\" fill=\"black\"/>\n", scale/5);
             }
 
-            if ( map[i][j].item == BATERIA                 ) {
-                fprintf(out, "<circle cx=\"");
-                fprintf(out, "%d", (i*scale));
-                fprintf(out, "\" cy=\"");
-                fprintf(out, "%d", (j*scale));
-                //puts("hue");
-                fprintf(out, "\" r=\"%d\" stroke=\"gold\" stroke-width=\"1\" fill=\"gold\"/>\n", scale/3);
-            } else if ( map[i][j].item == BRACO_SOLDA      ) {
-                fprintf(out, "<circle cx=\"");
-                fprintf(out, "%d", (i*scale));
-                fprintf(out, "\" cy=\"");
-                fprintf(out, "%d", (j*scale));
-                fprintf(out, "\" r=\"%d\" stroke=\"mediumspringgreen\" stroke-width=\"1\" fill=\"mediumspringgreen\"/>\n", scale/3);
-            } else if ( map[i][j].item == BOMBA            ) {
-                fprintf(out, "<circle cx=\"");
-                fprintf(out, "%d", (i*scale));
-                fprintf(out, "\" cy=\"");
-                fprintf(out, "%d", (j*scale));
-                fprintf(out, "\" r=\"%d\" stroke=\"steelblue\" stroke-width=\"1\" fill=\"steelblue\"/>\n", scale/3);
-            } else if ( map[i][j].item == REFRIGERACAO     ) {
-                fprintf(out, "<circle cx=\"");
-                fprintf(out, "%d", (i*scale));
-                fprintf(out, "\" cy=\"");
-                fprintf(out, "%d", (j*scale));
-                fprintf(out, "\" r=\"%d\" stroke=\"pink\" stroke-width=\"1\" fill=\"pink\"/>\n", scale/3);
-            } else if ( map[i][j].item == BRACO_PNEUMATICO ) {
-                fprintf(out, "<circle cx=\"");
-                fprintf(out, "%d", (i*scale));
-                fprintf(out, "\" cy=\"");
-                fprintf(out, "%d", (j*scale));
-                fprintf(out, "\" r=\"%d\" stroke=\"peru\" stroke-width=\"1\" fill=\"peru\"/>\n", scale/3);
+            if ( map[i][j].isItem ) {
+                if ( map[i][j].item == BATERIA                 ) {
+                    fprintf(out, "<circle cx=\"");
+                    fprintf(out, "%d", (i*scale));
+                    fprintf(out, "\" cy=\"");
+                    fprintf(out, "%d", (j*scale));
+                    //puts("hue");
+                    fprintf(out, "\" r=\"%d\" stroke=\"gold\" stroke-width=\"1\" fill=\"gold\"/>\n", scale/3);
+                } else if ( map[i][j].item == BRACO_SOLDA      ) {
+                    fprintf(out, "<circle cx=\"");
+                    fprintf(out, "%d", (i*scale));
+                    fprintf(out, "\" cy=\"");
+                    fprintf(out, "%d", (j*scale));
+                    fprintf(out, "\" r=\"%d\" stroke=\"mediumspringgreen\" stroke-width=\"1\" fill=\"mediumspringgreen\"/>\n", scale/3);
+                } else if ( map[i][j].item == BOMBA            ) {
+                    fprintf(out, "<circle cx=\"");
+                    fprintf(out, "%d", (i*scale));
+                    fprintf(out, "\" cy=\"");
+                    fprintf(out, "%d", (j*scale));
+                    fprintf(out, "\" r=\"%d\" stroke=\"steelblue\" stroke-width=\"1\" fill=\"steelblue\"/>\n", scale/3);
+                } else if ( map[i][j].item == REFRIGERACAO     ) {
+                    fprintf(out, "<circle cx=\"");
+                    fprintf(out, "%d", (i*scale));
+                    fprintf(out, "\" cy=\"");
+                    fprintf(out, "%d", (j*scale));
+                    fprintf(out, "\" r=\"%d\" stroke=\"pink\" stroke-width=\"1\" fill=\"pink\"/>\n", scale/3);
+                } else if ( map[i][j].item == BRACO_PNEUMATICO ) {
+                    fprintf(out, "<circle cx=\"");
+                    fprintf(out, "%d", (i*scale));
+                    fprintf(out, "\" cy=\"");
+                    fprintf(out, "%d", (j*scale));
+                    fprintf(out, "\" r=\"%d\" stroke=\"peru\" stroke-width=\"1\" fill=\"peru\"/>\n", scale/3);
+                }
             }
-        }
-    }
 
-    for ( int i = 0 ; i < 5 ; i++ ){
-        int x = fabrica[i].pos.x;
-        int y = fabrica[i].pos.y;
-        if ( fabrica[i].item == BATERIA                 ) {
-            fprintf(out, "<line x1=\"%d\" y1=\"%d\" x2=\"%d\" y2=\"%d\" stroke=\"gold\" stroke-width=\"2\" />\n", (x * scale - 5), (y * scale - 5), (x * scale + 5), (y * scale + 5)) ;
-            fprintf(out, "<line x1=\"%d\" y1=\"%d\" x2=\"%d\" y2=\"%d\" stroke=\"gold\" stroke-width=\"2\" />\n", (x * scale + 5), (y * scale - 5), (x * scale - 5), (y * scale + 5)) ;
-        } else if ( fabrica[i].item == BRACO_SOLDA      ) {
-            fprintf(out, "<line x1=\"%d\" y1=\"%d\" x2=\"%d\" y2=\"%d\" stroke=\"mediumspringgreen\" stroke-width=\"2\" />\n", (x * scale - 5), (y * scale - 5), (x * scale + 5), (y * scale + 5)) ;
-            fprintf(out, "<line x1=\"%d\" y1=\"%d\" x2=\"%d\" y2=\"%d\" stroke=\"mediumspringgreen\" stroke-width=\"2\" />\n", (x * scale + 5), (y * scale - 5), (x * scale - 5), (y * scale + 5)) ;
-        } else if ( fabrica[i].item == BOMBA            ) {
-            fprintf(out, "<line x1=\"%d\" y1=\"%d\" x2=\"%d\" y2=\"%d\" stroke=\"steelblue\" stroke-width=\"2\" />\n", (x * scale - 5), (y * scale - 5), (x * scale + 5), (y * scale + 5)) ;
-            fprintf(out, "<line x1=\"%d\" y1=\"%d\" x2=\"%d\" y2=\"%d\" stroke=\"steelblue\" stroke-width=\"2\" />\n", (x * scale + 5), (y * scale - 5), (x * scale - 5), (y * scale + 5)) ;
-        } else if ( fabrica[i].item == REFRIGERACAO     ) {
-            fprintf(out, "<line x1=\"%d\" y1=\"%d\" x2=\"%d\" y2=\"%d\" stroke=\"pink\" stroke-width=\"2\" />\n", (x * scale - 5), (y * scale - 5), (x * scale + 5), (y * scale + 5)) ;
-            fprintf(out, "<line x1=\"%d\" y1=\"%d\" x2=\"%d\" y2=\"%d\" stroke=\"pink\" stroke-width=\"2\" />\n", (x * scale + 5), (y * scale - 5), (x * scale - 5), (y * scale + 5)) ;
-        } else if ( fabrica[i].item == BRACO_PNEUMATICO ) {
-            fprintf(out, "<line x1=\"%d\" y1=\"%d\" x2=\"%d\" y2=\"%d\" stroke=\"peru\" stroke-width=\"2\" />\n", (x * scale - 5), (y * scale - 5), (x * scale + 5), (y * scale + 5)) ;
-            fprintf(out, "<line x1=\"%d\" y1=\"%d\" x2=\"%d\" y2=\"%d\" stroke=\"peru\" stroke-width=\"2\" />\n", (x * scale + 5), (y * scale - 5), (x * scale - 5), (y * scale + 5)) ;
-        } else {
-            printf("Invalid enum!!\n");
-            abort();
+            int x = i;
+            int y = j;
+
+            if ( map[i][j].isFabrica ) {
+                if ( map[i][j].item == BATERIA                 ) {
+                    fprintf(out, "<line x1=\"%d\" y1=\"%d\" x2=\"%d\" y2=\"%d\" stroke=\"gold\" stroke-width=\"2\" />\n", (x * scale - 5), (y * scale - 5), (x * scale + 5), (y * scale + 5)) ;
+                    fprintf(out, "<line x1=\"%d\" y1=\"%d\" x2=\"%d\" y2=\"%d\" stroke=\"gold\" stroke-width=\"2\" />\n", (x * scale + 5), (y * scale - 5), (x * scale - 5), (y * scale + 5)) ;
+                } else if ( map[i][j].item == BRACO_SOLDA      ) {
+                    fprintf(out, "<line x1=\"%d\" y1=\"%d\" x2=\"%d\" y2=\"%d\" stroke=\"mediumspringgreen\" stroke-width=\"2\" />\n", (x * scale - 5), (y * scale - 5), (x * scale + 5), (y * scale + 5)) ;
+                    fprintf(out, "<line x1=\"%d\" y1=\"%d\" x2=\"%d\" y2=\"%d\" stroke=\"mediumspringgreen\" stroke-width=\"2\" />\n", (x * scale + 5), (y * scale - 5), (x * scale - 5), (y * scale + 5)) ;
+                } else if ( map[i][j].item == BOMBA            ) {
+                    fprintf(out, "<line x1=\"%d\" y1=\"%d\" x2=\"%d\" y2=\"%d\" stroke=\"steelblue\" stroke-width=\"2\" />\n", (x * scale - 5), (y * scale - 5), (x * scale + 5), (y * scale + 5)) ;
+                    fprintf(out, "<line x1=\"%d\" y1=\"%d\" x2=\"%d\" y2=\"%d\" stroke=\"steelblue\" stroke-width=\"2\" />\n", (x * scale + 5), (y * scale - 5), (x * scale - 5), (y * scale + 5)) ;
+                } else if ( map[i][j].item == REFRIGERACAO     ) {
+                    fprintf(out, "<line x1=\"%d\" y1=\"%d\" x2=\"%d\" y2=\"%d\" stroke=\"pink\" stroke-width=\"2\" />\n", (x * scale - 5), (y * scale - 5), (x * scale + 5), (y * scale + 5)) ;
+                    fprintf(out, "<line x1=\"%d\" y1=\"%d\" x2=\"%d\" y2=\"%d\" stroke=\"pink\" stroke-width=\"2\" />\n", (x * scale + 5), (y * scale - 5), (x * scale - 5), (y * scale + 5)) ;
+                } else if ( map[i][j].item == BRACO_PNEUMATICO ) {
+                    fprintf(out, "<line x1=\"%d\" y1=\"%d\" x2=\"%d\" y2=\"%d\" stroke=\"peru\" stroke-width=\"2\" />\n", (x * scale - 5), (y * scale - 5), (x * scale + 5), (y * scale + 5)) ;
+                    fprintf(out, "<line x1=\"%d\" y1=\"%d\" x2=\"%d\" y2=\"%d\" stroke=\"peru\" stroke-width=\"2\" />\n", (x * scale + 5), (y * scale - 5), (x * scale - 5), (y * scale + 5)) ;
+                } else {
+
+                }
+            }
         }
     }
 
